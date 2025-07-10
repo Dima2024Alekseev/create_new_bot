df635f7dd5093500fa5540cd7fcc124eb56bb743
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-07-10 23:35:43 +0700

    ddd

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'primer.env' });
 const { Telegraf } = require('telegraf');
 const connectDB = require('./config/db');
 const { handleStart } = require('./controllers/userController');
