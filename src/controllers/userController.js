3817be86d4b4d3b39c2039c71c822345b3f69bc7
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-07-11 14:15:48 +0700

    user_role

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,20 @@
 const User = require('../models/User');
 const { paymentDetails } = require('../utils/helpers');
-const { checkAdmin } = require('./adminController');
 
 exports.handleStart = async (ctx) => {
-  const { id, first_name } = ctx.from;
-  
-  // Проверяем режим админа
-  if (id === parseInt(process.env.ADMIN_ID) && checkAdmin(ctx)) {
+  const { id, first_name, last_name, username } = ctx.from;
+
+  if (id === parseInt(process.env.ADMIN_ID)) {
     return ctx.replyWithMarkdown(
       '👋 *Админ-панель*\n\n' +
       'Команды:\n' +
       '/check - Проверить заявки\n' +
-      '/stats - Статистика\n' +
-      '/switchmode - Переключиться в режим пользователя'
+      '/stats - Статистика'
     );
   }
 
   const user = await User.findOne({ userId: id });
-  
+
   if (user?.status === 'active') {
     return ctx.replyWithMarkdown(
       `✅ *Ваша подписка активна до ${user.expireDate.toLocaleDateString()}*\n\n` +
